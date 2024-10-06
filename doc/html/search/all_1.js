b84e3487a3f4c2e446537362fae49067be2f2882
Nikita Vaganov <nikita.e.vaganov@gmail.com>
2024-10-06 21:44:34 +0200

    Add auto-generated docs Add links to auto-generated docs to README.md

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deadline_0',['deadline',['../structyatq_1_1_timer_queue_1_1_timer_handle.html#a1224e18ad256ed11346dcbe3175b2cae',1,'yatq::TimerQueue::TimerHandle']]]
+];
---
+++ doc/html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['result_0',['result',['../structyatq_1_1_timer_queue_1_1_timer_handle.html#ac52fa3d315a351a413a6428344a9a3ac',1,'yatq::TimerQueue::TimerHandle']]]
+];
